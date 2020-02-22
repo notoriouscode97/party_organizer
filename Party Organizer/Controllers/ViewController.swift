761c3fd868ac448d552ffe426c1188eea70f1b50
Dusan Dimic <dusan@manigo.com>
2020-02-22 15:50:44 +0100

    Added cocoapods to project Added RxSwift library

---
+++ Party Organizer/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Party Organizer
+//
+//  Created by Dusan Dimic on 2/22/20.
+//  Copyright © 2020 Dusan Dimic. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
